fa809d84d41bedc96e9efd2bbd8fce10a14c08a2
Yisan Huang <anonymousyisan@gmail.com>
2024-04-10 18:11:07 +0800

    feat: update gradio

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import gradio as gr
 import chatgpt
 import time
 
+css = """
+.my-plus-button .plus-button { display: none; } 
+.my-scroll-hide .scroll-hide { margin-left: 0px; }
+"""
+
 def generate_context_messages(messages: list):
     context_messages = [
         # system role
@@ -34,17 +39,31 @@ def generate_response(chat_history):
         time.sleep(0.02)
         yield chat_history
 
-with gr.Blocks() as demo:
-    chatbot = gr.Chatbot()
-    msg = gr.Textbox()
-    clear = gr.Button("Clear")
+def add_message(history, message):
+    for x in message["files"]:
+        history.append(((x,), None))
+    if message["text"] is not None:
+        history.append((message["text"], None))
+    return history, gr.MultimodalTextbox(value=None, interactive=False)
+
+with gr.Blocks(css=css) as demo:
+    chatbot = gr.Chatbot(
+        [],
+        elem_id="chatbot",
+        bubble_full_width=False,
+    )
+    
+    chat_input = gr.MultimodalTextbox(interactive=True, file_types=["text"], placeholder="请输入内容 ...", show_label=False, elem_classes=["my-plus-button", "my-scroll-hide"])
+
+    clear = gr.Button("清空所有记录")
 
     def user(user_message, history):
         return "", history + [[user_message, None]]
+    
+    chat_msg = chat_input.submit(add_message, [chatbot, chat_input], [chatbot, chat_input])
+    bot_msg = chat_msg.then(generate_response, chatbot, chatbot, api_name="bot_response")
+    bot_msg.then(lambda: gr.MultimodalTextbox(interactive=True), None, [chat_input])
 
-    msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
-        generate_response, chatbot, chatbot
-    )
     clear.click(lambda: None, None, chatbot, queue=False)
     
 demo.queue()
